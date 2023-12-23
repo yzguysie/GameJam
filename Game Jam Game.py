4571896d3039cb79caa85e4bfcd4f2ec0dfbc187
“nathanv2” <boiculesedavid@gmail.com>
2023-12-22 21:38:52 -0800

    guys what happened lol

--- Game Jam Game.py
+++ Game Jam Game.py
@@ -24,6 +24,10 @@ portal_sprite = False
 
 screen_width, screen_height = 768, 432
 
+
+
+print("6es6")
+
 #MAKE "BUMP pads" that PUSH you like terracotta in minecraft (Like sideways jump pad or those thingies in edge)
 fps = 120
 tickrate = 120
